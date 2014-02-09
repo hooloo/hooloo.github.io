b435f7080b7cdd80b5886373b074e7c7c72ddd5c
Sneezry <lizhe@lizhe.org>
2014-02-10 00:29:43 +0800

    14021001

--- javascripts/404.js
+++ javascripts/404.js
@@ -1,2 +1,8 @@
+document.title = sitetitle;
 var path = location.pathname;
-location.href = '/#!' + path;
+if(rss && (path == '/rss' || path == '/rss/' || path == '/feed' || path == '/feed/' || path == '/atom' || path == '/atom/')){
+	location.href = rss;
+}
+else{
+	location.href = '/#!' + path;
+}
--- javascripts/getmd.js
+++ javascripts/getmd.js
@@ -1,23 +1,37 @@
 var path = decodeURIComponent(location.hash.substr(2));
-
+var page = 1;
+if(location.search.substr(1,19)=='_escaped_fragment_='){
+	path = decodeURIComponent(location.search.substr(20).split('&')[0]);
+}
+if(path == '/'){path = ''; window.history.replacetate(null, '', '/');page=1;}
+else if(path && !location.search){window.history.replaceState(null, '', '/#!'+path);}
 var converter = new Showdown.converter();
 var content = document.getElementById('content');
 var dis = document.getElementById('disqus_thread');
+var loading = document.getElementById('loading');
+var backhome = document.getElementById('backhome');
 var xmlhttp;
 var disqus_url;
-
-var disqus_shortname = 'hooloodemo';
-var hostbase = 'http://hooloo.github.io';
-var githubname = 'hooloo';
-var repos = 'hooloo.github.io';
+var kw;
+var postList;
+var pending;
+var commentscount = new Array();
 
 main();
 
 function main(){
-	if(path){
-		window.history.pushState(null, '', '/#!'+path);
-		disqus_url = hostbase + path;
-		content.innerHTML = 'loading...';
+	var disqusCounts = document.getElementsByName('commentscount');
+	for(var i=0; i<disqusCounts.length; i++){
+		commentscount[Number(disqusCounts[i].id.substr(5))] = disqusCounts[i].innerText;
+	}
+	content.innerHTML = '';
+	loading.style.display = 'block';
+	if(path.split('/')[1] == 'search'){
+		search(path.split('/')[2]);
+	}
+	else if(path && path.split('/')[1] != 'page'){
+		disqus_url = hostbase + lowerCase(path);
+		//disqus_url = disqus_url.toLowerCase();
 		showpost(path);
 		(function() {
             var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
@@ -26,14 +40,47 @@ function main(){
         })();
 	}
 	else{
-		var el = document.createElement('script');
-		el.src = 'https://api.github.com/repos/' + githubname + '/' + repos + '/contents/md?callback=showlist';
-		document.getElementsByTagName('head')[0].appendChild(el);
+		//backhome.style.display = 'none';
+		document.title = sitetitle;
+		if(postList){
+			showlist(postList);
+		}
+		else{
+			pending = true;
+			document.getElementById('takinglonger').style.display = 'none';
+			chktakinglonger();
+			var el = document.createElement('script');
+			el.src = 'https://api.github.com/repos/' + githubname + '/' + repos + '/contents/md?callback=showlist';
+			document.getElementsByTagName('head')[0].appendChild(el);
+		}
 	}
 }
 
+function home(){
+	path = '';
+	dis.style.display = 'none';
+	dis.innerHTML = '';
+	if(page==1){
+		window.history.pushState(null, '', '/');
+	}
+	else{
+		path = '/page/'+page;
+		window.history.pushState(null, '', '/#!/page/'+page);
+	}
+	main();
+}
+
+function lowerCase(path){
+	path = path.split('%');
+	newPath = path[0];
+	for(var i=1; i<path.length; i++){
+		newPath += '%'+path[i].substr(0,2).toLowerCase()+path[i].substr(2);
+	}
+	return newPath;
+}
+
 function loadXMLDoc(url){
-	xmlhttp=null;
+	var xmlhttp=null;
 	if (window.XMLHttpRequest){// code for IE7, Firefox, Opera, etc.
 		xmlhttp=new XMLHttpRequest();
 	}
@@ -41,55 +88,133 @@ function loadXMLDoc(url){
 		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
 	}
 	if (xmlhttp!=null){
-		xmlhttp.onreadystatechange=state_Change;
+		pending = true;
+		document.getElementById('takinglonger').style.display = 'none';
+		chktakinglonger();
+		xmlhttp.onreadystatechange = function (){
+			if (xmlhttp.readyState==4){// 4 = "loaded"
+				pending = false;
+				document.getElementById('takinglonger').style.display = 'none';
+				loading.style.display = 'none';
+				//backhome.style.display = 'block';
+				if (xmlhttp.status==200){// 200 = "OK"
+					var blog_text = xmlhttp.responseText;
+					var encoded = false;
+					if(blog_text.substr(0,2)=='::'){
+						blog_text = Base64.decode(blog_text.substr(2));
+						encoded = true;
+					};
+					var converter = new Showdown.converter();
+					content.innerHTML = '<div style="padding: 20px 20px 20px 40px;"><div id="back_home"><a href="/" onclick="home();return false;">'+sitetitle+'</a><span>&nbsp;›&nbsp;</span></div><div id="post_title">' + decodeUtf8(path.substr(1).split('/')[path.substr(1).split('/').length-1].replace(/_/g, ' ')) + (encoded?Base64.decode('PHN1cCBzdHlsZT0iZm9udC1zaXplOjAuNWVtO3ZlcnRpY2FsLWFsaWduOiBzdXBlcjsiIHRpdGxlPSLmraTmlofnq6Dlt7Looqvph43mlrDnvJbnoIHku6XourLpgb/lrqHmn6UiPuKYmuiiq+e8lueggeeahOWGheWuuTwvc3VwPg=='):'') + '</div>' + converter.makeHtml(blog_text) + '<div class="date">Posted at ' + pdate + '</div></div>';
+					if(dis){
+						dis.style.display = 'block';
+					}
+				}
+				else if(xmlhttp.status==404) {
+					document.title = 'Not Found - '+sitetitle;
+					content.innerHTML = '<img src="images/despicable_me.png" />';
+				}
+				else {
+					document.title = 'Technology Problem - '+sitetitle;
+					content.innerHTML = '<div id="takinglonger"><blockquote>We meet a problem when try to handle ' + path + ' (Err: ' + xmlhttp.status + ').</blockquote></div>';
+				}
+			}
+		}
 		xmlhttp.open("GET",url,true);
 		xmlhttp.send(null);
 	}
-	else{
-		alert("Your browser does not support XMLHTTP.");
-	}
 }
 
-function state_Change(){
-	if (xmlhttp.readyState==4){// 4 = "loaded"
-		if (xmlhttp.status==200){// 200 = "OK"
-			var converter = new Showdown.converter();
-			content.innerHTML = converter.makeHtml(xmlhttp.responseText);
-			if(dis){
-				dis.style.display = 'block';
-			}
+function chktakinglonger(){
+	setTimeout(function(){
+		if(pending){
+			document.getElementById('takinglonger').style.display = 'block';
 		}
-		else if(xmlhttp.status==404) {
-			content.innerHTML = '<img src="/images/404.jpg" />';
-		}
-		else {
-			content.innerHTML = 'We meet a problem.';
-		}
-	}
+	}, 10000);
 }
 
 function showpost(path){
-	//window.history.pushState(null, path.substr(1).split('/')[path.substr(1).split('/').length-1] + ' - Sneezry', path);
 	var url = location.protocol + '//' + location.hostname + '/md/' + path.substr(1).replace(/\//g, '-');
-	document.title = path.substr(1).split('/')[path.substr(1).split('/').length-1] + ' - Hooloo';
+	document.title = decodeUtf8(path.substr(1).split('/')[path.substr(1).split('/').length-1].replace(/_/g, ' ')) + ' - '+sitetitle;
+	pdate = path.substr(1).split('/')[0]+'-'+path.substr(1).split('/')[1]+'-'+path.substr(1).split('/')[2];
 	loadXMLDoc(url);
 }
 
 function showlist(list){
-	var txt = '';
-	for(var i = list.data.length; i > 0; i--){
-		txt += '<h2><a href="/#!/' + list.data[i-1].name.replace(/-/g, '/') + '">' + list.data[i-1].name.split('-')[list.data[i-1].name.split('-').length-1] + '</a></h2>';
+	if(path.split('/')[1] == 'page'){
+		page = Number(path.split('/')[2]);
+		if(isNaN(page) || page < 1){
+			page = 1;
+		}
+		if(page == 1){
+			window.history.replaceState(null, '', '/');
+		}
 	}
-	content.innerHTML = converter.makeHtml(txt);
+	pending = false;
+	document.getElementById('takinglonger').style.display = 'none';
+	postList = list;
+	var txt = '';
+	if(page*20>list.data.length){
+		page = Math.ceil(list.data.length/20);
+		window.history.replaceState(null, '', '/#!/page/'+page);
+	}
+	for(var i = list.data.length-(page-1)*20; i > 0 && i > list.data.length-page*20; i--){
+		txt += '<postlist><a href="/#!/' + list.data[i-1].name.replace(/-/g, '/') + '">' + list.data[i-1].name.split('-')[list.data[i-1].name.split('-').length-1].replace(/_/g, ' ') + '</a><div class="post_info"><span class="post_date">Posted at '+list.data[i-1].name.split('-')[0]+'-'+list.data[i-1].name.split('-')[1]+'-'+list.data[i-1].name.split('-')[2]+'</span><span class="disqus_count"><a href="' + hostbase + '/' + encodePath(list.data[i-1].name) + (commentscount[i]?'':'#disqus_thread') + '" name="commentscount" id="post-'+i+'">'+(commentscount[i]?commentscount[i]:'')+'</a></span></div></postlist>';
+	}
+	if(page==1 && page*20<list.data.length){
+		txt += '<postlist><a class="prev_page" href="/#!/page/'+(page+1)+'">←较早的文章</a><div style="clear:both"></div></postlist>';
+	}
+	else if(page>1 && page*20>=list.data.length){
+		txt += '<postlist><a class="next_page" href="/#!/page/'+(page-1)+'">较新的文章→</a><div style="clear:both"></div></postlist>';
+	}
+	else if(page>1 && page*20<list.data.length){
+		txt += '<postlist><a class="prev_page" href="/#!/page/'+(page+1)+'">←较早的文章</a><a class="next_page" href="/#!/page/'+(page-1)+'">较新的文章→</a><div style="clear:both"></div></postlist>';
+	}
+	loading.style.display = 'none';
+	content.innerHTML = txt;
+	(function () {
+        var s = document.createElement('script'); s.async = true;
+		s.type = 'text/javascript';
+        s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+        (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+    }());
 }
 
-window.onpopstate = function(event){
+function encodePath(path){
+  path = encodeURIComponent(path).replace(/-/g, '/');
+  for(var i=0; i<path.length; i++){
+    if(path.substr(i,1) == '%'){
+      path = path.substr(0,i+1)+path.substr(i+1,2).toLowerCase()+path.substr(i+3);
+      i+=2;
+    }
+  }
+  return path;
+}
+
+function decodeUtf8(str){
+	try{
+		var tmp = decodeURIComponent(str);
+		if(tmp==str){
+			return str;
+		}
+		else{
+			return decodeUtf8(tmp);
+		}
+	}
+	catch(e){
+		return str;
+	}
+}
+
+window.onhashchange = function(){
 	if(location.hash && location.hash.substr(1,1) != '!'){
-		window.history.pushState(null, '', '/#!'+path);
+		window.history.replaceState(null, '', '/#!'+path);
 		return;
 	}
+	goToTop();
 	dis.style.display = 'none';
 	dis.innerHTML = '';
 	path = location.hash.substr(2);
+	if(path == '/'){path = ''; window.history.replaceState(null, '', '/');}
 	main();
 }
